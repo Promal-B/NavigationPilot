28d6b706b28cc229209799a561a5b32021982711
Promal <promal.office@gmail.com>
2025-07-14 10:58:54 +0600

    Updated git ignore and package dependency

--- Package.swift
+++ Package.swift
@@ -5,17 +5,30 @@ import PackageDescription
 
 let package = Package(
     name: "NavigationPilot",
+    platforms: [
+        .iOS(.v14)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "NavigationPilot",
             targets: ["NavigationPilot"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/canopas/UIPilot.git", .upToNextMajor(from: "2.0.2")),
+        .package(url: "https://github.com/SimplyDanny/SwiftLintPlugins", exact: "0.58.2")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "NavigationPilot"),
-
+            name: "NavigationPilot",
+            dependencies: [
+                .product(name: "UIPilot", package: "uipilot"),
+            ],
+            plugins: [
+                .plugin(name: "SwiftLintBuildToolPlugin", package: "SwiftLintPlugins")
+            ]
+        )
     ]
 )
